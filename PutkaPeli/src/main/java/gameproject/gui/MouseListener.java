c987c15829d14424dd7a807a9917e2f02ac2037c
Toni Kainulainen <tonikainulainen.ttk@hotmail.fi>
2020-04-12 17:29:01 +0300

    Scetching mouseadaptation

---
+++ PutkaPeli/src/main/java/gameproject/gui/MouseListener.java
@@ -0,0 +1,14 @@
+package gameproject.gui;
+
+import java.awt.event.MouseAdapter;
+// ehkä kevyempi on javax.swing kirjastosta ottaa
+
+public class MouseListener extends MouseAdapter {
+
+	/* Toiminta-idea
+	 * Klikatessa näytölle piirtyy näkymätön tai hyvin pieni muoto,
+	 * jonka avulla voidaan tarkistaa risteävätkö juuri piirretty muoto
+	 * ja klikkauksen alueella oleva muoto. Kun muodot risteävät on true,
+	 * niin se tarkoittaa, että muoto on valittu.
+	 * */
+}
