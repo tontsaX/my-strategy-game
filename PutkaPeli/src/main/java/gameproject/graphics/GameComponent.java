305c12779e66df83715530bdd23007de2fbd4589
Toni Kainulainen <tonikainulainen.ttk@hotmail.fi>
2020-04-18 11:34:51 +0300

    Start of selected feature

--- PutkaPeli/src/main/java/gameproject/graphics/GameComponent.java
+++ PutkaPeli/src/main/java/gameproject/graphics/GameComponent.java
@@ -5,10 +5,15 @@ import java.awt.Graphics;
 public abstract class GameComponent {
 
 	protected int x, y;
+	protected boolean selected;
 	
 	public GameComponent(int x, int y) {
 		this.x = x;
 		this.y = y;
+	}
+	
+	public void setSelected(boolean selected) {
+		this.selected = selected;
 	}
 
 	public abstract void update();
