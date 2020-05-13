19af9292b3c726a8160b34c4817b1df37e6ea4f1
Toni Kainulainen <tonikainulainen.ttk@hotmail.fi>
2020-05-13 13:26:50 +0300

    Velocity is updated to original after lag adjustment.

--- MyStrategyGame/src/main/java/gameproject/graphics/GameObject.java
+++ MyStrategyGame/src/main/java/gameproject/graphics/GameObject.java
@@ -2,21 +2,16 @@ package gameproject.graphics;
 
 public abstract class GameObject extends GameComponent {
 	
+	private final int orgVelX, orgVelY;
 	protected int velocityX, velocityY;
 
 	public GameObject(int x, int y, int width, int height, int velX, int velY) {
 		super(x, y, width, height);
 		this.velocityX = velX;
 		this.velocityY = velY;
+		orgVelX = velocityX;
+		orgVelY = velocityY;
 	}
-
-	/*
-	@Override
-	public void render(Graphics g) {
-		// TODO Auto-generated method stub
-		
-	}
-	*/
 	
 	// sets new velocity which is updated on the next cycle
 	/* vika, jos velocity on 1 ja lag update on 2 velocity päivitetään
@@ -24,10 +19,21 @@ public abstract class GameObject extends GameComponent {
 	 * taas jälkeen 2, niin velocityksi tulee 4 jne. Velocity ei palaudu alkuperäiseen
 	 * tällä hetkellä. */
 	public void updateVelocity(double velocity) {
+		setVelocityX(orgVelX);
+		setVelocityY(orgVelY);
+		
 		if(velocity >= 1) {
 			velocityX *= (int) velocity;
 			velocityY *= (int) velocity;
 		}
+	}
+	
+	public void setVelocityX(double velX) {
+		velocityX = (int)velX;
+	}
+	
+	public void setVelocityY(double velY) {
+		velocityY = (int) velY;
 	}
 	
 }
